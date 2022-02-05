48ddad43a4914ce4588b2f5558517968230d92cc
Gianluca Lucchesi <gianluca@Gianlucas-MacBook-Pro.local>
2022-02-05 12:47:15 +0100

    Added user input check

--- src/main/java/com/capgemini/iCalConvert/ICalConvertApplication.java
+++ src/main/java/com/capgemini/iCalConvert/ICalConvertApplication.java
@@ -8,13 +8,22 @@ import java.util.Scanner;
 @SpringBootApplication
 public class ICalConvertApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter input calendar file path: ");
 		String path = scanner.nextLine();
+
+		if (!path.contains(".ics")) {
+			throw new Exception("Invalid path");
+		}
+
 		System.out.println("Enter UID of last imported event: ");
 		String uid = scanner.nextLine();
+
+		if (uid == "" || uid == null) {
+			throw new Exception("UID cannot be null");
+		}
 
 		iCal4j ical4j = new iCal4j(path, uid);
 		ical4j.setOldCalendar();
