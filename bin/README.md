845efc34207e97528fef7d32cd742961316e7d8a
Lucchesi <gianluca.lucchesi@capgemini.com>
2022-02-09 18:07:36 +0100

    Used file picker + redesign

---
+++ bin/README.md
@@ -0,0 +1,6 @@
+# CalendarConverter
+
+Compares an old and new .ics (calendar) file and creates a new .ics keeping only the new events.
+
+## Created runnable jar containing JavaFX
+https://www.youtube.com/watch?v=busqPViapBQ&t=336s
--- src/main/java/com/capgemini/visualcalendarconverter/Controller.java
+++ src/main/java/com/capgemini/visualcalendarconverter/Controller.java
@@ -2,38 +2,72 @@ package com.capgemini.visualcalendarconverter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
 
 public class Controller {
     @FXML
-    private TextField oldPathTextField;
+    private ImageView validOldFile;
     @FXML
-    private TextField newPathTextField;
+    private ImageView validNewFile;
+    @FXML
+    private ImageView conversionDone;
 
     String oldPath;
     String newPath;
 
     @FXML
-    public void onSubmit(ActionEvent actionEvent) {
-        oldPath = this.oldPathTextField.getText();
-        newPath = this.newPathTextField.getText();
+    public void onConvert(ActionEvent actionEvent) {
+        this.oldPath = this.oldPath.replace('"', ' ');
+        this.oldPath = this.oldPath.trim();
+        this.newPath = this.newPath.replace('"', ' ');
+        this.newPath = this.newPath.trim();
 
-        runConversion(oldPath, newPath);
-    }
-
-    private void runConversion(String oldPath, String newPath) {
-        oldPath = oldPath.replace('"', ' ');
-        oldPath = oldPath.trim();
-        newPath = newPath.replace('"', ' ');
-        newPath = newPath.trim();
-
-        Ical4j ical4j = new Ical4j(oldPath, newPath);
+        Ical4j ical4j = new Ical4j(this.oldPath, this.newPath);
         ical4j.setCalendars();
         ical4j.compareCalendars();
         ical4j.printCalendarEvents(ical4j.getCalendarToImport());
         ical4j.export();
-        ical4j.deleteFile(oldPath);
-        ical4j.renameFile(newPath, oldPath);
+        this.conversionDone.setVisible(true);
+        ical4j.deleteFile(this.oldPath);
+        ical4j.renameFile(this.newPath, this.oldPath);
     }
+
+    @FXML
+    private void onClickBtnOld(ActionEvent actionEvent) {
+        String username = System.getProperty("user.name");
+
+        FileChooser fc = new FileChooser();
+        fc.setInitialDirectory(new File("C:\\Users\\" + username +"\\Downloads"));
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("iCal Files", "*.ics"));
+        File selectedFile = fc.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            this.oldPath = selectedFile.getAbsolutePath();
+            this.validOldFile.setVisible(true);
+        }
+    }
+
+    @FXML
+    private void onClickBtnNew(ActionEvent actionEvent) {
+        String username = System.getProperty("user.name");
+
+        FileChooser fc = new FileChooser();
+        fc.setInitialDirectory(new File("C:\\Users\\" + username +"\\Downloads"));
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("iCal Files", "*.ics"));
+        File selectedFile = fc.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            this.newPath = selectedFile.getAbsolutePath();
+            this.validNewFile.setVisible(true);
+        }
+    }
+
 }
